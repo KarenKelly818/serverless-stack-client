d3e92fec8eeebf02f78beaf741acc70349de8568
Karen Kelly <karen.kelly@alchemysystems.com>
2020-08-21 12:39:26 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-karen-kelly"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://dx2pzeezyk.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_3JCNs4npO",
+    APP_CLIENT_ID: "3hr35bi906qaub5moh4d5i3hvc",
+    IDENTITY_POOL_ID: "us-east-2:e0640dac-a56b-4c56-8ed4-ac2d977f9d0d"
+  }
+};
